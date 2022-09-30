02db4e44feda3f8bdfd91264bf9d1eec32947cb7
YoonSuhyeok <cooltime15@likelion.org>
2022-09-30 16:57:21 +0900

    elsint & prettier 설정 및 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,62 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+      '@react-native-community',
+      'airbnb',
+      'airbnb/hooks',
+      'airbnb-typescript',
+      'prettier',
+      'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  parserOptions: {
+      project: './tsconfig.json',
+  },
   overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
+      {
+          files: ['*.ts', '*.tsx'],
+          rules: {
+              '@typescript-eslint/no-shadow': ['error'],
+              'no-shadow': 'off',
+              'no-undef': 'off',
+          },
       },
-    },
   ],
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+      'react/function-component-definition': [
+          2,
+          { namedcomponents: 'arrow-function' },
+      ],
+      "@typescript-eslint/indent": "off",
+      "react/jsx-indent": "off",
+      "react/jsx-indent-props": "off",
+      "react/jsx-closing-bracket-location": "off",
+      "import/extensions": [
+          "error",
+          "ignorePackages",
+          {
+            "js": "never",
+            "jsx": "never",
+            "ts": "never",
+            "tsx": "never"
+          }
+      ],
+      'prettier/prettier': [
+          'error',
+          {
+              endOfLine: 'auto'
+          },
+      ],
+      'import/extensions': 'off',
+      'import/no-unresolved': 'off',
+      "import/order": ["error",
+          { 
+              "groups" : [ "external", "builtin", "internal", "sibling", "parent", "index" ],
+              "alphabetize": { "order": "asc" },
+              "newlines-between": "always"
+          }
+      ]
+  },
 };
