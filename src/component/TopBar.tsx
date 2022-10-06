a580e6adeb1e57117fcd927d87896d5297fe7f7c
YoonSuhyeok <cooltime15@likelion.org>
2022-10-06 17:23:33 +0900

    Add MainPage

---
+++ src/component/TopBar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TopBar() {
+    return <div>Hello</div>;
+}
+
+export default TopBar;
---
+++ src/page/MainPage.tsx
@@ -0,0 +1,16 @@
+import BottomBox from '@component/BottomBox';
+import MidBox from 'component/MidBox';
+import TopBar from 'component/TopBar';
+import React from 'react';
+
+function MainPage() {
+    return (
+        <>
+            <TopBar />
+            <MidBox />
+            <BottomBox />
+        </>
+    );
+}
+
+export default MainPage;
